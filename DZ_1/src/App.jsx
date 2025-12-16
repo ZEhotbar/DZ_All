62bc79de459be8347d96ad7832eb4bbda870e64c
David <dmitrienkodavid1401@gmail.com>
2025-12-16 23:49:57 +0600

    ваше сообщение

---
+++ DZ_1/src/App.jsx
@@ -0,0 +1,14 @@
+import Square from './companents/Square'; 
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+      <h1>Мой квадрат</h1>
+      <Square/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
